0c5378e117afff6daf7cf0ef9738f7565e85c451
wangguan111 <wang_guan111@126.com>
2019-01-04 00:26:01 +0800

    eureka

---
+++ eureka/eurekaClient/src/main/java/com/example/eureka_client/EurekaClientApplication.java
@@ -0,0 +1,14 @@
+package com.example.eureka_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+}
+
